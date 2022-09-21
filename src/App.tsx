0c378edc49338f53d1e01b329f698684050e3941
ahsankhan201010 <ahsankhan201010@gmail.com>
2022-09-21 16:20:13 +0300

    setup metamask for CRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [accountAddress, setAccountAddress] = useState();
+  const connectWallet = async () => {
+    if (window.ethereum && window.ethereum.request) {
+      const account = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccountAddress(account[0]);
+    } else {
+      console.log("install meta mask");
+    }
+  };
+
+  useEffect(() => {
+    if (window.ethereum && window.ethereum.request) {
+      window.ethereum
+        .request({
+          method: "eth_accounts",
+        })
+        .then((account) => {
+          if (account.length > 0) {
+            setAccountAddress(account[0]);
+          }
+        });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wallet">
+        {!accountAddress && <button onClick={connectWallet}>click</button>}
+        {accountAddress && <h1>Account Address: {accountAddress}</h1>}
+      </div>
     </div>
   );
 }
